5d56a4e99645bd9c7cb4dc3afe2008ad9a070b42
hangyan <yanhanglyf@163.com>
2015-04-23 11:17:59 +0800

    add README

---
+++ README.md
@@ -0,0 +1,34 @@
+# Docker Remote Attch Use Websocket Example
+This is a simple example of docker remote attach use websocket protocol.
+
+## Install
+`go get github.com/hangyan/docker-ws-client`
+
+## Usage
+![](https://raw.github.com/hangyan/docker-ws-client/master/ws.png)
+
+The first argument is the container id,and second is the command you want
+to execute in that container
+
+## Notes
+The commands that has stream output may interfere with the later commmand's
+output,like `top`,`ping`,so you need to restart the container after you execute
+these commands.
+
+
+## Others
+You can also use a web browser to act as a websocket client too,there is
+a chrome extension can do this : [Simple Websocket Client](https://chrome.google.com/webstore/detail/simple-websocket-client/pfdhoblngboilpfeibdedpjgfnlcodoo?hl=en)
+
+
+![](https://raw.github.com/hangyan/docker-ws-client/master/extension.png)
+
+
+Notes:
+
+1. You shoud add linebreak when you input the command
+2. the example's (`ls -al`) output has some strange chars,they are terminal's
+color control character.
+3. Don't use `logs=1` or the output will be messed up.
+
+
--- main.go
+++ main.go
@@ -2,14 +2,56 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
+	"time"
+
+	"golang.org/x/net/websocket"
+)
+
+const (
+	WS_BASE_URL = "ws://localhost:4243"
+	ORIGIN      = "http://localhost"
 )
 
 func main() {
+	if len(os.Args) != 3 {
+		fmt.Println("Usage : <program>  <contain id> <cmd>")
+		return
+	}
 
 	id := os.Args[1]
 	cmd := os.Args[2]
-	wsURL := fmt.Sprintf("ws://localhost:4243/v1.17/containers/%s/attach/ws?logs=0&stream=1&stdin=1&stdout=1&stderr=1", id)
-	UseXNet(wsURL, cmd)
 
+	wsURL := fmt.Sprintf("%s/v1.17/containers/%s/attach/ws?logs=0&stream=1&stdin=1&stdout=1&stderr=1", WS_BASE_URL, id)
+	useXNet(wsURL, cmd)
+}
+
+func useXNet(url string, cmd string) {
+
+	fmt.Printf("URL : %s\n", url)
+	ws, err := websocket.Dial(url, "", ORIGIN)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer ws.Close()
+
+	message := []byte(cmd + "\n")
+	fmt.Printf("Send: %s\n", message)
+
+	if _, err := ws.Write(message); err != nil {
+		log.Fatal(err)
+	}
+
+	ws.SetReadDeadline(time.Now().Add(1 * time.Second))
+
+	for {
+		var msg = make([]byte, 512)
+		var n int
+		if n, err = ws.Read(msg); err != nil {
+			break // usually is time out
+		}
+		fmt.Printf("%s", msg[:n])
+		msg = nil
+	}
 }
